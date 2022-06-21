c6b5bbac1b0f33d46b672ea306b4c81fa665ce78
Maximilian Hänsel <mh@maxhaensel.de>
2022-06-21 12:17:53 +0200

    js typo

--- infrastructure/api/infrastructure.py
+++ infrastructure/api/infrastructure.py
@@ -51,7 +51,7 @@ class GraphQLEndpoint(Construct):
                 "event_bus_name": eventHub.bus.event_bus_name,
             },
             bundling=go_lambda.BundlingOptions(command_hooks=CommandHooks()),
-            moduleDir="infrastructure/lambda/graph-ql-api",
+            module_dir="infrastructure/lambda/graph-ql-api",
         )
         # func = lambda_.Function(
         #     self,
